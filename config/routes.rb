fd5b6c63806f9ce22af6ad5ddd4c89dc20d99e0b
julien <ju.jaffre@gmail.com>
2020-03-03 14:25:10 +0000

    manage users route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+
+  unauthenticated do
+    devise_scope :user do
+      root to: 'devise/sessions#new', as: :unauthentificated_root
+    end
+  end
+
+  authenticated :user do
+    root to: 'pages#home'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :categories do
     resources :goals, only: [:new, :create, :delete]
   end
-
 end
+
