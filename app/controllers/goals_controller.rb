d5ac68b1731acd3a6cb7067a6e19d4568109cb43
Jennifer Tran <hellojennifertran@gmail.com>
2020-03-03 04:53:53 -0800

    Created goals routes, views, and seeds - view still WIP

---
+++ app/controllers/goals_controller.rb
@@ -0,0 +1,24 @@
+class GoalsController < ApplicationController
+  def index
+    @goal = Goal.all
+  end
+
+  def create
+    @goal = Goal.new(goal_params)
+    if @goal.save
+      redirect_to goal_path(@goal.id)
+    else
+      render :new
+    end
+  end
+
+  def new
+    @goal = Goal.new
+  end
+
+  private
+
+  def goal_params
+    params.require(:goal).permit(:name, :date, :user_id, :category_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/goals', to: 'goals#index'
+  post '/goals', to: 'goals#create'
+
+  get '/goals/new', to: 'goals#new', as: :new_goal
+  # get 'goals/:id/edit', to: 'goals#edit', as: :edit_goal
+
+  # get '/goals/:id', to: 'goals#show', as: :goal
+  # put 'goals/:id', to: 'goals#update'
+  # patch 'goals/:id', to: 'goals#update'
+  # delete 'goals/:id', to: 'goals#destroy'
 end
