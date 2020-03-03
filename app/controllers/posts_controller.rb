5db79625c5399fdcf00b6f980b5f7d84909ad8ce
michael lim <michael.limnh@gmail.com>
2020-03-03 12:33:40 +0000

    added post controller and routes

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,22 @@
+class PostsController < ApplicationController
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :description, :date)
+  end
+
+end
