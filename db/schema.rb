547b28d581459f685216c28792e4a08a27d32354
michael lim <michael.limnh@gmail.com>
2020-03-04 14:28:54 +0000

    added category_type to goals table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_03_165420) do
+ActiveRecord::Schema.define(version: 2020_03_04_142228) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,9 +30,9 @@ ActiveRecord::Schema.define(version: 2020_03_03_165420) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "category_type"
     t.string "category"
     t.text "note"
+    t.string "category_type"
     t.index ["user_id"], name: "index_goals_on_user_id"
   end
 
