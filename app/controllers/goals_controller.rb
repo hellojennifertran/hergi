ac0df5574fc53827056672a476b451584a7c6cd4
michael lim <michael.limnh@gmail.com>
2020-03-04 17:43:34 +0000

    privatized goal list with checkbox

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -16,9 +16,21 @@ class GoalsController < ApplicationController
     @goal = Goal.new
   end
 
+  def update
+    @goal.update(goal_params)
+  end
+
+  def privatize_all
+    private_goals = params[:privatize_goals][:goal_ids].map{ |id| Goal.find(id) }
+    private_goals.each { |goal| goal.update(private: true) }
+    public_goals = current_user.goals - private_goals
+    public_goals.each { |goal| goal.update(private: false) }
+    redirect_to profile_path(current_user)
+  end
+
   private
 
   def goal_params
-    params.require(:goal).permit(:category, :category_type, :date, :note)
+    params.require(:goal).permit(:category, :category_type, :date, :note, :private)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,11 @@ Rails.application.routes.draw do
   get '/goals', to: 'goals#index'
   post '/goals', to: 'goals#create'
   get '/goals/new', to: 'goals#new', as: :new_goal
+  resources :goals, only: [:index, :create, :new] do
+    collection do
+      patch :privatize_all
+    end
+  end
   # get 'goals/:id/edit', to: 'goals#edit', as: :edit_goal
   # get '/goals/:id', to: 'goals#show', as: :goal
   # put 'goals/:id', to: 'goals#update'
