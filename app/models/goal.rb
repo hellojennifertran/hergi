c86f4a520f6e944589c1b241df24b7ce0800cf45
Jennifer Tran <hellojennifertran@gmail.com>
2020-03-04 07:12:31 -0800

    Can create a new goal now!

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,3 +1,18 @@
 class Goal < ApplicationRecord
   belongs_to :user
+
+  CATEGORIES = [
+    {
+      category: 'Health',
+      category_type: %w[sport healthyfood running]
+    }, 
+    {
+      category: 'Exploration',
+      category_type: %w[dancing climbing swimming]
+    },
+    {
+      category: 'Relationships',
+      category_type: %w[friends family dating social]
+    }
+  ]
 end
