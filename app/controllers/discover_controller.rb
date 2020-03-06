345eb78f0c2fe8808071047e3558d9a8536cd4cb
Jennifer Tran <hellojennifertran@gmail.com>
2020-03-06 07:35:39 -0800

    Added controllers and view for discover

---
+++ app/controllers/discover_controller.rb
@@ -0,0 +1,17 @@
+require 'net/http'
+require 'uri'
+require 'json'
+
+class GetEvents
+  def call
+    # call api
+    # token = '4qgQOYNjfU2vJKPv9JnRF2CTVnA24Kr2kiImdw3v'
+    # uri = URI.parse("https://api.predicthq.com/v1/events/")
+    # http = Net::HTTP.new(uri.host)
+    # request = Net::HTTP::Get.new(uri.request_uri)
+
+
+    # # request['Authorization'] = "Token AMfd48QtwsU4NAhvajb06jDCImAKnWBRSQFltp6q"
+    # response = http.request(request)
+  end
+end
