bc1f3f2a5263b2e0fa9f8a4f0e92e2b61049ca38
Jennifer Tran <hellojennifertran@gmail.com>
2020-03-06 09:43:14 -0800

    chaged avatar picture

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,7 @@ module Hergi
         end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.autoload = :classic
     # config.autoload_paths << "#{Rails.root}/app/services"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
