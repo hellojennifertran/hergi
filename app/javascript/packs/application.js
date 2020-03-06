0dfb8e2fb8d525249d5558a6df8c14c247fc7b34
Cerian Smith <ceri.smith@yahoo.de>
2020-03-06 14:58:58 +0000

    added header and fixed other small bugs

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,29 @@ import "bootstrap";
 
 const description = document.querySelector(".edit-profile");
 
-description.addEventListener("click", (e) => {
-  const descriptionForm = document.querySelector('.description-form');
-  descriptionForm.classList.toggle("invisible-form")
-})
+if (description) {
+  description.addEventListener("click", (e) => {
+    const descriptionForm = document.querySelector('.description-form');
+    descriptionForm.classList.toggle("invisible-form")
+  })
+}
+
+
+
+
+// header
+
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollpos = window.pageYOffset + 50;
+window.onscroll = function() {
+  if (window.pageYOffset > 10) {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+      document.getElementById("myHeader").style.top = "0";
+    } else {
+      document.getElementById("myHeader").style.top = "-54px";
+    }
+    prevScrollpos = currentScrollPos;
+
+  }
+}
