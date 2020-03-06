097e54ee29d0a114513ae3c7d870cc2bd7dc42ef
michael lim <michael.limnh@gmail.com>
2020-03-06 12:52:07 +0000

    json-discover-api

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ module Hergi
         end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
+    config.autoload_paths << "#{Rails.root}/app/services"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
