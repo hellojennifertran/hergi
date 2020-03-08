908bf263364c0508a68bfda231875c0e15d88993
Jennifer Tran <hellojennifertran@gmail.com>
2020-03-08 06:49:28 -0700

    Added discovery cards using predicthq apis

---
+++ app/models/discover.rb
@@ -0,0 +1,2 @@
+class Discover < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_05_124622) do
+ActiveRecord::Schema.define(version: 2020_03_08_113557) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,11 @@ ActiveRecord::Schema.define(version: 2020_03_05_124622) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "discovers", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "goals", force: :cascade do |t|
