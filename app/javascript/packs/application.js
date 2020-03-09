61ea215812b7fc7a17b8d64387dcd71f6d1651b5
michael lim <michael.limnh@gmail.com>
2020-03-09 14:19:14 +0000

    added [privatize feature to form

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,155 +1,110 @@
-// require("@rails/ujs").start()
-// require("@rails/activestorage").start()
-// require("channels")
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
 
-// import "bootstrap";
-// import Chart from 'chart.js';
+import "bootstrap";
+import Chart from 'chart.js';
 
-// const description = document.querySelector(".edit-profile");
-// if (description != null){
-// description.addEventListener("click", (e) => {
-//   const descriptionForm = document.querySelector('.description-form');
-//   descriptionForm.classList.toggle("invisible-form")
-// })
-// }
+document.addEventListener('turbolinks:load', () => {
+  const editProfile = document.querySelector(".edit-profile");
+  if (editProfile) {
+    editProfile.addEventListener("click", (e) => {
+      const descriptionForm = document.querySelector('.user-note');
+      descriptionForm.classList.toggle("invisible-form");
+      const checkBoxes = document.querySelectorAll('.goal-privatize');
+      checkBoxes.forEach((checkbox) => {
+        checkbox.classList.toggle('invisible-form');
+      })
+      const updateBtn = document.querySelector('.profile-edit-btn');
+      updateBtn.classList.toggle('invisible-form');
+    })
+  }
 
-// // dashboard graph
-// const chart = document.getElementById('myChart');
-// var ctx = document.getElementById('myChart').getContext('2d');
-// var myChart = new Chart(ctx, {
-//     type: 'doughnut',
-//     data: {
-//         labels: ['Health done', "Health To-do", 'Exploration done', "Exploration To-do", 'Relationship done', "Relationship  To-do"],
-//         datasets: [{
-//             label: '# of Votes',
-//             data: [chart.dataset.healthDone, chart.dataset.healthTobedone, chart.dataset.explorationDone, chart.dataset.explorationTobedone, chart.dataset.relationshipDone, chart.dataset.relationshipTobedone],
-//             backgroundColor: [
-//                 '#ff0101',
-//                 '#ec9999',
-//                 '#167ffb',
-//                 '#8ebbf0',
-//                 '#1edd88',
-//                 '#91d3b6'
-//             ],
-//             borderColor: [
-//                 '#D6D6D6',
-//                 '#D6D6D6',
-//                 '#D6D6D6',
-//                 '#D6D6D6',
-//                 '#D6D6D6',
-//                 '#D6D6D6'
-//             ],
-//             borderWidth: 0
-//         }]
-//     },
-//     options: {
-//             rotation: 1 * Math.PI,
-//             circumference: 1 * Math.PI,
-//             responsive: true,
-//             legend: {
-//               position: 'top',
-//             },
-//             title: {
-//               display: true,
-//               text: 'Monthly Goals'
-//             },
-//             animation: {
-//               animateScale: true,
-//               animateRotate: true
-//             }
-//           }
-// });
+  const userDashboard = document.getElementById("myChart");
+  if (userDashboard) {
+    // dashboard graph
+    const chart = document.getElementById('myChart');
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+            labels: ['Health done', 'Health unfinished', 'Exploration done', 'Exploration unfinished', 'Relationship done', 'Relationship unfinished'],
+            datasets: [{
+                label: '# of Votes',
+                data: [chart.dataset.healthDone, chart.dataset.healthTobedone, chart.dataset.explorationDone, chart.dataset.explorationTobedone, chart.dataset.relationshipDone, chart.dataset.relationshipTobedone],
+                backgroundColor: [
+                    '#FF008A',
+                    '#FFFFFF',
+                    '#040083',
+                    '#FFFFFF',
+                    '#27CA00',
+                    '#FFFFFF'
+                ],
+                borderColor: [
+                    '#D6D6D6',
+                    '#D6D6D6',
+                    '#D6D6D6',
+                    '#D6D6D6',
+                    '#D6D6D6',
+                    '#D6D6D6'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+                rotation: 1 * Math.PI,
+                circumference: 1 * Math.PI,
+                responsive: true,
+                legend: {
+                  position: 'top',
+                },
+                title: {
+                  display: true,
+                  text: 'Chart.js Doughnut Chart'
+                },
+                animation: {
+                  animateScale: true,
+                  animateRotate: true
+                }
+              }
+    });
 
-// const editProfile = document.querySelector(".edit-profile");
+    // /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+    // var prevScrollpos = window.pageYOffset + 50;
+    // window.onscroll = function() {
+    //   if (window.pageYOffset > 10) {
+    //     var currentScrollPos = window.pageYOffset;
+    //     if (prevScrollpos > currentScrollPos) {
+    //       document.getElementById("myHeader").style.top = "0";
+    //     } else {
+    //       document.getElementById("myHeader").style.top = "-54px";
+    //     }
+    //     prevScrollpos = currentScrollPos;
 
+    //   }
+    // }
+  }
 
-// editProfile.addEventListener("click", (e) => {
-//   const descriptionForm = document.querySelector('.description-form');
-//   descriptionForm.classList.toggle("invisible-form")
-// })
+  // // api call
+  // const discovers = document.getElementById("discovers-index");
+  // if (discovers) {
+  //   const url = "https://api.predicthq.com/v1/events/?q=concert";
+  //   const options = {
+  //     mode: 'cors',
+  //     headers: {
+  //       'Authorization':'Bearer fp8j4_bwRRRWgxLZNMJPIqt_9P6mh3fUIvGTLhEf'
+  //     }
+  //   }
+  //   fetch(url, options)
+  //     .then(response => response.json())
+  //     .then((data) => {
+  //       data.results.forEach((res) => {
+  //         // interpolate .insertadjacetnhtml
+  //         // const discoverBody = 
+  //         // const html = 
+  //       })
+  //     })
+  // }
 
-// const userDashboard = document.getElementById("myChart");
-// if (userDashboard) {
-//   // dashboard graph
-//   const chart = document.getElementById('myChart');
-//   var ctx = document.getElementById('myChart').getContext('2d');
-//   var myChart = new Chart(ctx, {
-//       type: 'doughnut',
-//       data: {
-//           labels: ['Health done', 'Health unfinished', 'Exploration done', 'Exploration unfinished', 'Relationship done', 'Relationship unfinished'],
-//           datasets: [{
-//               label: '# of Votes',
-//               data: [chart.dataset.healthDone, chart.dataset.healthTobedone, chart.dataset.explorationDone, chart.dataset.explorationTobedone, chart.dataset.relationshipDone, chart.dataset.relationshipTobedone],
-//               backgroundColor: [
-//                   '#FF008A',
-//                   '#FFFFFF',
-//                   '#040083',
-//                   '#FFFFFF',
-//                   '#27CA00',
-//                   '#FFFFFF'
-//               ],
-//               borderColor: [
-//                   '#D6D6D6',
-//                   '#D6D6D6',
-//                   '#D6D6D6',
-//                   '#D6D6D6',
-//                   '#D6D6D6',
-//                   '#D6D6D6'
-//               ],
-//               borderWidth: 1
-//           }]
-//       },
-//       options: {
-//               rotation: 1 * Math.PI,
-//               circumference: 1 * Math.PI,
-//               responsive: true,
-//               legend: {
-//                 position: 'top',
-//               },
-//               title: {
-//                 display: true,
-//                 text: 'Chart.js Doughnut Chart'
-//               },
-//               animation: {
-//                 animateScale: true,
-//                 animateRotate: true
-//               }
-//             }
-//   });
-
-//   /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
-//   var prevScrollpos = window.pageYOffset + 50;
-//   window.onscroll = function() {
-//     if (window.pageYOffset > 10) {
-//       var currentScrollPos = window.pageYOffset;
-//       if (prevScrollpos > currentScrollPos) {
-//         document.getElementById("myHeader").style.top = "0";
-//       } else {
-//         document.getElementById("myHeader").style.top = "-54px";
-//       }
-//       prevScrollpos = currentScrollPos;
-
-//     }
-//   }
-// }
-
-// // api call
-// const discovers = document.getElementById("discovers-index");
-// if (discovers) {
-//   const url = "https://api.predicthq.com/v1/events/?q=concert";
-//   const options = {
-//     mode: 'cors',
-//     headers: {
-//       'Authorization':'Bearer fp8j4_bwRRRWgxLZNMJPIqt_9P6mh3fUIvGTLhEf'
-//     }
-//   }
-//   fetch(url, options)
-//     .then(response => response.json())
-//     .then((data) => {
-//       data.results.forEach((res) => {
-//         // interpolate .insertadjacetnhtml
-//         // const discoverBody = 
-//         // const html = 
-//       })
-//     })
-// }
+})
--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/dashboard', to: 'users#dashboard', as: :dashboard
   get '/profile/:id', to: 'users#profile', as: :profile
-  get '/update/:id', to: 'users#update', as: :update
+  patch '/update/:id', to: 'users#update', as: :update
   get '/discover', to: 'discovers#index', as: :discovers
 
   resources :goals, only: [:index, :create, :new] do
