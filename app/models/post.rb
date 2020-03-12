1d1e9a5fa647777372ad40ee8f4fe7f6b5d97d1b
julien <ju.jaffre@gmail.com>
2020-03-12 15:31:05 +0000

    delete post with comment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   has_one_attached :photo
   belongs_to :user
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
   validates :content, presence: true
 end
