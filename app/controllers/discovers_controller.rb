8867ad5398e5fd5ce79022ae557a486ea9a389a3
michael lim <michael.limnh@gmail.com>
2020-03-12 15:38:05 +0000

    added saved events page

--- app/controllers/discovers_controller.rb
+++ app/controllers/discovers_controller.rb
@@ -1,6 +1,26 @@
 class DiscoversController < ApplicationController
-  def index
 
+  before_action :read_files
+
+  def index; end
+
+  def saved
+    @json_events = [@events_health, @events_exploration, @events_relationships]
+    @all_events = {}
+    @json_events.each do |json|
+      json["events"].each do |event|
+        @all_events["#{event['id']}"] = event
+      end
+    end
+
+    @saved_events = current_user.events.map do |event|
+      @all_events["#{event}"]
+    end
+  end
+
+  private
+
+  def read_files
     film_json = File.read(Rails.root.join('app/assets/json/film-response.json'))
     festivals_json = File.read(Rails.root.join('app/assets/json/london-festivals-response.json'))
     sports_json = File.read(Rails.root.join('app/assets/json/london-sports-active-life-response.json'))
@@ -9,6 +29,6 @@ class DiscoversController < ApplicationController
     @events_health = JSON.parse(sports_json)
     @events_exploration = JSON.parse(arts_json)
     @events_relationships = JSON.parse(festivals_json)
-
   end
+
 end
